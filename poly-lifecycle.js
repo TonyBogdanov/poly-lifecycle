458f9b59b3eefe7795398b962a8602139d2c6eb5
Tony Bogdanov <tonybogdanov@gmail.com>
2017-07-05 12:43:11 +0300

    smaller filesize?

--- poly-lifecycle.js
+++ poly-lifecycle.js
@@ -3,54 +3,55 @@
 
     window.TB = window.TB || {};
     (TB.Lifecycle = function () {
-        this.hasFired = {
-            'DOMContentLoaded': false,
-            'WebComponentsReady': false
+        var a = 'DOMContentLoaded';
+        var b = 'WebComponentsReady';
+
+        this.fired = {
+            a: false,
+            b: false
         };
 
         this.listeners = [];
 
-        this.notify = function (e) {
-            this.hasFired[e.type] = true;
-            for (var i = 0; i < this.listeners.length; i++) {
-                var shouldFire = true;
-                for (var j = 0; j < this.listeners[i][0].length; j++) {
-                    if (!this.hasFired[this.listeners[i][0][j]]) {
-                        shouldFire = false;
-                        break;
-                    }
+        this.hasFired = function (events) {
+            for (var i = 0; i < events.length; i++) {
+                if (!this.fired[events[i]]) {
+                    return false;
                 }
-                if (shouldFire) {
+            }
+            return true;
+        };
+
+        this.notify = function (e) {
+            this.fired[e.type] = true;
+            for (var i = 0; i < this.listeners.length; i++) {
+                if (this.hasFired(this.listeners[i][0])) {
                     this.listeners[i][1]();
                 }
             }
         };
 
-        this.onDOMReady = function (callback) {
-            if (this.hasFired['DOMContentLoaded']) {
+        this.on = function (events, callback) {
+            if (this.hasFired(events)) {
                 callback();
             } else {
-                this.listeners.push([['DOMContentLoaded'], callback]);
+                this.listeners.push([events, callback]);
             }
+        };
+
+        this.onDOMReady = function (callback) {
+            this.on([a], callback);
         };
 
         this.onWCReady = function (callback) {
-            if (this.hasFired['WebComponentsReady']) {
-                callback();
-            } else {
-                this.listeners.push([['WebComponentsReady'], callback]);
-            }
+            this.on([b], callback);
         };
 
         this.onReady = function (callback) {
-            if (this.hasFired['DOMContentLoaded'] && this.hasFired['WebComponentsReady']) {
-                callback();
-            } else {
-                this.listeners.push([['DOMContentLoaded', 'WebComponentsReady'], callback]);
-            }
+            this.on([a, b], callback);
         };
 
-        document.addEventListener('DOMContentLoaded', this.notify.bind(this), {once: true});
-        document.addEventListener('WebComponentsReady', this.notify.bind(this), {once: true});
+        document.addEventListener(a, this.notify.bind(this), {once: true});
+        document.addEventListener(b, this.notify.bind(this), {once: true});
     }).call(TB.Lifecycle);
 })();
